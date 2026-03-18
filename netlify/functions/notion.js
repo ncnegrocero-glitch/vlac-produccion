396a535180d910e717d8f3e1856b5161098876c8
ncnegrocero-glitch <nc.negrocero@gmail.com>
2026-03-18 14:52:04 -0300

    Update notion.js

--- netlify/functions/notion.js
+++ netlify/functions/notion.js
@@ -1,8 +1,11 @@
 const https = require('https');
 
 exports.handler = async (event) => {
-  const path = event.path.replace('/.netlify/functions/notion', '');
-  const url = `https://api.notion.com/v1${path}`;
+  const path = event.path.replace('/.netlify/functions/notion', '') || '/';
+  const query = event.rawQuery ? '?' + event.rawQuery : '';
+  const url = `https://api.notion.com/v1${path}${query}`;
+  
+  console.log('Proxying:', event.httpMethod, url);
   
   return new Promise((resolve) => {
     const options = {
@@ -19,10 +22,18 @@ exports.handler = async (event) => {
       res.on('data', chunk => data += chunk);
       res.on('end', () => resolve({
         statusCode: res.statusCode,
-        headers: { 'Access-Control-Allow-Origin': '*' },
+        headers: { 
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/json'
+        },
         body: data
       }));
     });
+    
+    req.on('error', (e) => resolve({
+      statusCode: 500,
+      body: JSON.stringify({ error: e.message })
+    }));
     
     if (event.body) req.write(event.body);
     req.end();
